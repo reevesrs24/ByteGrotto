6b2275b50c2550d9301d3e26209f0e3bf6c27fe2
Ryan Reeves <reevesrs24@gmail.com>
2022-05-30 19:55:22 -0400

    Refactored add_code_cave

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ByteGrotto import ByteGrotto
 
 
 def main():
-    bg = ByteGrotto(pe_path="calc.exe", pe_ouput_name="new.exe")
+    bg = ByteGrotto(pe_path="../Mallard/MLSEC_2021_malware/010", pe_ouput_name="new.exe")
     bg.generate_adversarial_pe()
 
 if __name__ == '__main__':
